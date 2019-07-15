eaaf2a9e3d2f4274a25ea14d56368c7febd4d21c
sidneydijkstra <sidney0099@hotmail.com>
2019-07-15 17:50:37 +0200

    added bluetooth script

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+
+from bluetooth import *
+
+print("INIT")
+
+nearby_devices = discover_devices(lookup_names = True)
+
+print ("found %d devices" % len(nearby_devices))
+
+for name, addr in nearby_devices:
+     print (" %s - %s" % (addr, name))
